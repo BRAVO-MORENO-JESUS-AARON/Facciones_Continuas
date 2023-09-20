e7336d954aae4a35de5e6293a997bb0508200af5
AaronBM <LIS22110089@irapuato.tecnm.mx>
2023-09-19 19:29:42 -0600

    Agregar archivos de proyecto.

---
+++ Facciones_Continuas/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Facciones_Continuas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            int a, b, i = 1;
+            String ecuacion = "";
+            a = int.Parse(TxtNum1.Text);
+            b = int.Parse(TxtNum2.Text);
+
+            while (b != 0)
+            {
+                
+                int temp = b;
+                b = a%b;
+                if (b == 0)
+                {
+                    ecuacion = a + "/"+temp +" = " +(a/temp);
+                }
+                else
+                {
+                    ecuacion = a + "/" + temp + " = " + (a / temp) + " + 1/" + temp + " / " + b;
+                }
+                TbDatos.Rows.Add(i, a+ " divido entre " + temp+" es "+(a/temp)+ " y sobran "+b, ecuacion );
+                a = temp;
+                i++;
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+         
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TxtNum1.Text = "";
+            TxtNum2.Text = "";
+            TbDatos.Rows.Clear();
+        }
+    }
+}
